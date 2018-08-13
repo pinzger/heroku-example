7835846c25cd4a565ae48fd57f6b6b29f4900747
Martin Pinzger <martin.pinzger@aau.at>
2018-08-13 11:12:19 +0200

    small change to the app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express()
 
 // The code below will display 'Hello World How is it going!' to the browser when you go to http://localhost:3000
 app.get('/', function (req, res) {
-  res.send('Hello World!') 
+  res.send('This is an example using Heroku to deploy a node.js web app') 
 })
 
 var port = process.env.PORT || '3000';
